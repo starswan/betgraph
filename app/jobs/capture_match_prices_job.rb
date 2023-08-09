aee7f558dab937c1305dbb2fffde19a8aaf78e7c
starswan <github@starswan.com>
2023-08-09 10:10:41 +0100

    Betfair stream API

--- app/jobs/capture_match_prices_job.rb
+++ app/jobs/capture_match_prices_job.rb
@@ -5,15 +5,18 @@ class CaptureMatchPricesJob < BetfairJob
 
   MarketId = Struct.new(:exchangeId, :marketId, keyword_init: true)
 
-  def perform(match)
-    dbmarkets = match.bet_markets.live
-    market_ids = dbmarkets.map { |bm| MarketId.new(exchangeId: bm.exchange_id, marketId: bm.marketid).freeze }
-    prices = bc.getMultipleMarketPrices market_ids
-    # this puts the prices in the same order as the dbmarkets list
-    ordered_prices = market_ids.map { |m| prices.detect { |p| p.fetch(:marketId) == "#{m.exchangeId}.#{m.marketId}" } }
-    mpt = MarketPriceTime.create! time: Time.zone.now
-    dbmarkets.zip(ordered_prices).each do |dbmarket, market_price|
-      ProcessMarketPriceJob.perform_later dbmarket, market_price, mpt
+  def perform(_match)
+    bc.stream.each do |item|
+      logger.info "Receeived stream item #{item}"
     end
+    # dbmarkets = match.bet_markets.live
+    # market_ids = dbmarkets.map { |bm| MarketId.new(exchangeId: bm.exchange_id, marketId: bm.marketid).freeze }
+    # prices = bc.getMultipleMarketPrices market_ids
+    # # this puts the prices in the same order as the dbmarkets list
+    # ordered_prices = market_ids.map { |m| prices.detect { |p| p.fetch(:marketId) == "#{m.exchangeId}.#{m.marketId}" } }
+    # mpt = MarketPriceTime.create! time: Time.zone.now
+    # dbmarkets.zip(ordered_prices).each do |dbmarket, market_price|
+    #   ProcessMarketPriceJob.perform_later dbmarket, market_price, mpt
+    # end
   end
 end
