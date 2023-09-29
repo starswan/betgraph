0bc20a459cfa9842b16e2bc2f0b7176a695b65f9
Stephen Dicks <github@starswan.com>
2023-09-29 08:06:11 +0100

    RVM ubuntu package changes (#226)

--- clock.sh
+++ clock.sh
@@ -7,7 +7,7 @@ export HOME=`echo ~`
 source $HOME/.bash_login
 dirname=`dirname $0`
 cd $dirname
-source $HOME/.rvm/environments/ruby-3.1.4@bg
+source $RVM_DIR/environments/ruby-3.1.4@bg
 bundle check || bundle install
 program='clock'
 pidfile="tmp/pids/$program.pid"
