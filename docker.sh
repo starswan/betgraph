13bfa8606a97ae90c69846ad3750513059721b52
starswan <github@starswan.com>
2023-10-03 22:26:15 +0100

    docker

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
 # run docker against a volume created with docker volume create
 #
-docker run --name timescaledb -d -p 5432:5432 -v pgdata:/home/postgres/pgdata/data -e POSTGRES_PASSWORD=password timescale/timescaledb-ha:pg14-latest
+docker container prune -f
+docker run --name timescaledb -p 5432:5432 -v pgdata:/home/postgres/pgdata/data -e POSTGRES_PASSWORD=password timescale/timescaledb-ha:pg14-latest
