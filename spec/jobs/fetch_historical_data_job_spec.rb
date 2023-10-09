ac68e4d4e496e1c5105fd468a319c36ee2e5a08a
starswan <github@starswan.com>
2023-10-09 23:44:08 +0100

    coverage for finding division on data load

--- spec/jobs/fetch_historical_data_job_spec.rb
+++ spec/jobs/fetch_historical_data_job_spec.rb
@@ -12,10 +12,10 @@ RSpec.describe FetchHistoricalDataJob, :vcr, type: :job do
   end
 
   let(:soccer_match) do
-    create(:soccer_match, kickofftime: Time.zone.local(2020, 2, 12, 12, 30, 0),
+    create(:soccer_match, kickofftime: Time.zone.local(2020, 2, 12, 12, 29, 0),
                           division: division,
                           result: build(:result, homescore: 3, awayscore: 0),
-                          name: "Milton Keynes Dons v Cheltenham")
+                          name: "MK Dons v Cheltenham")
   end
 
   before do
@@ -28,13 +28,31 @@ RSpec.describe FetchHistoricalDataJob, :vcr, type: :job do
                           division: division,
                           result: build(:result, homescore: 4, awayscore: 2),
                           name: "Preston v Aston Villa")
+
+    create(:soccer_match, kickofftime: Time.zone.local(2018, 4, 3, 19, 45, 0),
+                          division: division,
+                          result: build(:result, homescore: 1, awayscore: 2),
+                          name: "Aston Villa v Reading")
+
+    create(:team_name, name: "Fulham", team: build(:team, sport: soccer))
+    create(:team_name, name: "Leeds", team: build(:team, sport: soccer))
+    create(:team_name, name: "Bolton", team: build(:team, sport: soccer))
+    create(:team_name, name: "Birmingham", team: build(:team, sport: soccer))
   end
 
-  it "creates markets and prices" do
+  it "creates prices for first of november" do
     expect {
       expect {
         described_class.perform_now Date.new(2017, 11, 1), "GB"
       }.to change(BetMarket, :count).by(2)
     }.to change(MarketPrice, :count).by(20)
+  end
+
+  it "creates prices for third of april" do
+    expect {
+      expect {
+        described_class.perform_now Date.new(2018, 4, 3), "GB"
+      }.to change(BetMarket, :count).by(2)
+    }.to change(MarketPrice, :count).by(1318)
   end
 end
