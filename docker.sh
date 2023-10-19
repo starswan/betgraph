ef26afe55eeb1d3420c7bdd9cc60e82d11fd4ae2
starswan <github@starswan.com>
2023-10-19 19:40:32 +0100

    Port to postgres database

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@
 # $Id$
 #
 # run docker against a volume created with docker volume create
-docker run --rm --name betgraph_db -m 1G -p 5433:5432 -v betgraph_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
+docker run --rm --name betgraph -m 1G -p 5434:5432 -v betgraph:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
 
