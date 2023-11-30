0c0270e7d170e33d12196383dcca20943837be42
starswan <github@starswan.com>
2023-11-30 19:18:10 +0000

    fill historical data holes

--- app/jobs/load_historical_data_job.rb
+++ app/jobs/load_historical_data_job.rb
@@ -165,7 +165,7 @@ private
           while timestamp - last_time > 1.minute
             last_time += 1.minute
             last_mpt = MarketPriceTime.create! time: last_time, created_at: last_time
-            runner.market_prices.create! back1price: runner.market_prices.last.back1price || runner_change.fetch(:ltp), market_price_time: last_mpt
+            runner.market_prices.create! back1price: runner.market_prices.any? ? runner.market_prices.last.back1price : runner_change.fetch(:ltp), market_price_time: last_mpt
           end
         end
         mpt = MarketPriceTime.create! time: timestamp, created_at: timestamp if mpt.blank?
