14e2ede1d9e7283a6db0e6e791bcd29d71d870e7
starswan <github@starswan.com>
2023-12-18 07:30:47 +0000

    tweak docker version of pg timescale

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 # run docker against a volume created with docker volume create
 #
 docker container prune -f
-docker run --name timescaledb -p 5432:5432 -v pgdata:/home/postgres/pgdata/data -e POSTGRES_PASSWORD=password timescale/timescaledb-ha:pg14-all
+docker run --name timescaledb -p 5432:5432 -v pgdata:/home/postgres/pgdata/data -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
