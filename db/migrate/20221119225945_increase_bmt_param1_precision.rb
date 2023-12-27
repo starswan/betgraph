9f1d13716094df20852d50625c7c3414d5620171
starswan <github@starswan.com>
2023-12-27 13:49:22 +0000

    Remove old migrations up to 2023

--- db/migrate/20221119225945_increase_bmt_param1_precision.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class IncreaseBmtParam1Precision < ActiveRecord::Migration[6.0]
-  def change
-    change_column :betfair_market_types, :param1, :decimal, precision: 7, scale: 2
-  end
-end
