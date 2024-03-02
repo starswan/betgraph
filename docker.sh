a903b9b83b37262c268d8b8fded2906cf3d87cd9
starswan <github@starswan.com>
2024-03-02 14:40:05 +0000

    smaller timescale image - same as chessdb

--- docker.sh
+++ docker.sh
@@ -1,7 +1,6 @@
-# run docker against a volume created with docker volume create
 #
-#docker container prune -f
-# This version doesm't seem to work properly when timescaledb starts up
-#docker run --name timescaledb -p 5432:5432 -v pgdata:/home/postgres/pgdata/data -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
-docker run --name betgraph_db -p 5433:5432 -v betgraph_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password timescale/timescaledb-ha:pg14-all
+# $Id$
+#
+# run docker against a volume created with docker volume create
+docker run --rm --name betgraph_db -m 1G -p 5433:5432 -v betgraph_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
 
