8a24ccde646f53ff7a495d97d881a3cf0f7c49c5
starswan <github@starswan.com>
2024-03-10 12:19:42 +0000

    tweak tickle in controller

--- app/controllers/soccer_matches_controller.rb
+++ app/controllers/soccer_matches_controller.rb
@@ -59,7 +59,7 @@ class SoccerMatchesController < ApplicationController
 
   def update
     respond_to do |format|
-      live_count = SoccerMatch.future.live_priced.count
+      live_count = BetMarket.live.count
       if @football_match.update(update_match_params)
         TickleLivePricesJob.perform_later if live_count.zero? && @football_match.live_priced
         flash[:notice] = "SoccerMatch was successfully updated."
