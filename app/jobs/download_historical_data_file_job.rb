52884fcc33c1bdc7576c53e22d62adf78f6077cd
starswan <github@starswan.com>
2024-03-10 16:31:02 +0000

    tweak

--- app/jobs/download_historical_data_file_job.rb
+++ app/jobs/download_historical_data_file_job.rb
@@ -5,18 +5,16 @@ class DownloadHistoricalDataFileJob < BetfairJob
   queue_priority PRIORITY_LOAD_FOOTBALL_DATA
 
   def perform(filename)
-    data_stream = StringIO.new(bc.download_file(filename))
-
-    data = ""
-    Bzip2::FFI::Reader.open(data_stream) do |reader|
-      while (buffer = reader.read(8192))
-        # process uncompressed bytes in buffer
-        data += buffer
+    data = Enumerator.new do |yielder|
+      Bzip2::FFI::Reader.open(StringIO.new(bc.download_file(filename))) do |reader|
+        while (buffer = reader.read(8192))
+          yielder << buffer
+        end
       end
     end
 
     event_id = filename.split("/")[-2]
-    StringIO.new(data).each_line(chomp: true) do |line|
+    StringIO.new(data.to_a.join).each_line(chomp: true) do |line|
       LoadHistoricalDataJob.perform_later event_id, JSON.parse(line, symbolize_names: true)
     end
   end
