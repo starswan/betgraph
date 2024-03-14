95ece4b2ab2b0a9d523880e194597fa5f3493fa9
starswan <github@starswan.com>
2024-03-14 09:08:48 +0000

    fix cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -93,6 +93,13 @@ namespace :bundler do
   end
 end
 
+namespace :yarn do
+  task :install, roles: :app do
+    run "yarn:install"
+  end
+end
+
 after "deploy:update_code", "bundler:symlink_bundled_gems"
 before "deploy:assets:precompile", "bundler:install"
+before "deploy:assets:precompile", "yarn:install"
 after "deploy:update_code", "deploy:migrate"
