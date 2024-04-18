ece0fd392f6b909a6526bbb6daf991fe57148e77
starswan <github@starswan.com>
2024-04-18 19:10:49 +0100

    remove pg limit

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #
 # $Id$
 #
-# run docker against a volume created with docker volume create
-docker run --rm --name betgraph_db -m 1G -p 5433:5432 -v betgraph_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
-
+# Need to remove the memory limitation when restoring a backup from MySql
+docker run -d --rm --name starswan_betgraph_postgres -m 1G -p 5433:5432 -v starswan_betgraph:/var/lib/postgresql/data -e TZ=London -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
+#docker run -d --rm --name starswan_betgraph_postgres -p 5433:5432 -v starswan_betgraph:/var/lib/postgresql/data -e TZ=London -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg14
