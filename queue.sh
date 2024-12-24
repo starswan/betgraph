a14c39ded84dfc8b53178263107feaef4f3806f4
starswan <github@starswan.com>
2024-12-24 09:27:36 +0000

    ruby 3.2.6

--- queue.sh
+++ queue.sh
@@ -7,7 +7,7 @@ export HOME=`echo ~`
 source $HOME/.bash_login
 dirname=`dirname $0`
 cd $dirname
-source $RVM_DIR/environments/ruby-3.1.6@bg
+source $RVM_DIR/environments/ruby-3.2.6@bg
 bundle check || bundle install
 program='queue'
 pidfile="tmp/pids/$program.pid"
