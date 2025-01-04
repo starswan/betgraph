dd4a356e9c21059218cf18a2a1f69ed8627d47cf
Stephen Dicks <github@starswan.com>
2025-01-04 21:06:43 +0000

    fixup betfair market types and double chance value (#533)

--- spec/valuers/soccer/double_chance_spec.rb
+++ spec/valuers/soccer/double_chance_spec.rb
@@ -6,7 +6,53 @@
 require "rails_helper"
 
 RSpec.describe Soccer::DoubleChance do
-  it "has a value method with 4 parameters" do
-    expect(described_class.new.value(1, 0, 3, 0)).to eq(0)
+  let(:homescore) { 3 }
+
+  context "with home win" do
+    let(:awayscore) { 0 }
+
+    it "is positive with home or away" do
+      expect(described_class.new.value(0, 0, homescore, awayscore)).to eq(1)
+    end
+
+    it "is positive with home or draw" do
+      expect(described_class.new.value(0, -1, homescore, awayscore)).to eq(1)
+    end
+
+    it "is negative with away or draw" do
+      expect(described_class.new.value(-1, 0, homescore, awayscore)).to eq(-1)
+    end
+  end
+
+  context "with away win" do
+    let(:awayscore) { 4 }
+
+    it "is positive with home or away" do
+      expect(described_class.new.value(0, 0, homescore, awayscore)).to eq(1)
+    end
+
+    it "is negative with home or draw" do
+      expect(described_class.new.value(0, -1, homescore, awayscore)).to eq(-1)
+    end
+
+    it "is positive with away or draw" do
+      expect(described_class.new.value(-1, 0, homescore, awayscore)).to eq(1)
+    end
+  end
+
+  context "with draw" do
+    let(:awayscore) { 3 }
+
+    it "is negative with home or away" do
+      expect(described_class.new.value(0, 0, homescore, awayscore)).to eq(-1)
+    end
+
+    it "is positive with home or draw" do
+      expect(described_class.new.value(0, -1, homescore, awayscore)).to eq(1)
+    end
+
+    it "is positive with away or draw" do
+      expect(described_class.new.value(-1, 0, homescore, awayscore)).to eq(1)
+    end
   end
 end
