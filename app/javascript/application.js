4479473afa237728b41319e72e428f91ab9bad92
starswan <github@starswan.com>
2025-02-11 20:00:18 +0000

    move js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,7 +11,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require Chart.bundle
-//= require chartkick
-//= require_tree .
+// require jquery
+// require Chart.bundle
+// require chartkick
+// require_tree .
+
+import $ from "jquery";
+window.$ = $;
+
+import './active_admin'
--- esbuild.config.js
+++ esbuild.config.js
@@ -9,10 +9,10 @@ coffee_options = { transpile: { "presets": ["@babel/preset-env"] } }
 // the absWorkingDirectory set below allows us to use paths relative to that location
 esbuild.build({
     // entryPoints: ['./application.js', './*.coffee'],
-    entryPoints: ['./application.js', './bet_markets.coffee', './betfair_events.coffee', './market_price_times.coffee', './matches.coffee'],
+    entryPoints: ['application.js', 'bet_markets.coffee', 'betfair_events.coffee', 'market_price_times.coffee', 'matches.coffee'],
     bundle: true,
     outdir: path.join(process.cwd(), "app/assets/builds"),
-    absWorkingDir: path.join(process.cwd(), "app/assets/javascripts"),
+    absWorkingDir: path.join(process.cwd(), "app/javascript"),
     watch: process.argv.includes("--watch"),
     sourcemap: true,
     plugins: [
