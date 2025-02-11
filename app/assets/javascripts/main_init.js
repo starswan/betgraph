6f73f5af47257c16696df0ed8d3c0afcfd6c4138
starswan <github@starswan.com>
2025-02-11 16:57:49 +0000

    fixup jquery ijmkport

--- app/assets/javascripts/main_init.js
+++ app/assets/javascripts/main_init.js
@@ -3,7 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("jquery");
+import $ from "jquery";
+window.$ = $;
 
 import Rails from "@rails/ujs";
 Rails.start();
