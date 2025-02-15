43e20b08a0c0da5b63d26d0ba5c0cf9f7661a779
starswan <github@starswan.com>
2025-02-15 09:36:41 +0000

    add activeadmin to esbuild

--- esbuild.config.js
+++ esbuild.config.js
@@ -9,7 +9,7 @@ coffee_options = { transpile: { "presets": ["@babel/preset-env"] } }
 // the absWorkingDirectory set below allows us to use paths relative to that location
 esbuild.build({
     // entryPoints: ['./application.js', './*.coffee'],
-    entryPoints: ['application.js', 'bet_markets.coffee', 'betfair_events.coffee', 'market_price_times.coffee', 'matches.coffee'],
+    entryPoints: ['application.js', 'active_admin.js', 'bet_markets.coffee', 'betfair_events.coffee', 'market_price_times.coffee', 'matches.coffee'],
     bundle: true,
     outdir: path.join(process.cwd(), "app/assets/builds"),
     absWorkingDir: path.join(process.cwd(), "app/javascript"),
