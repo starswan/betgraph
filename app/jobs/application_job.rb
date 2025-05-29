2089691e15c73656ba8e35ce45ea0f4de2cbf0e3
starswan <github@starswan.com>
2025-05-29 19:59:37 +0100

    bump priority

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -19,7 +19,7 @@ class ApplicationJob < ActiveJob::Base
   # Not worth retrying a badly-formed job
   discard_on ArgumentError
 
-  OFFSET_PRI = 56_000
+  OFFSET_PRI = 55_000
 
   PRIORITY_EXECUTE_TRADE = OFFSET_PRI + 5
   PRIORITY_CLOSE_DEAD_MARKETS = OFFSET_PRI + 10
@@ -39,9 +39,10 @@ class ApplicationJob < ActiveJob::Base
   # Lower than lowest BetfairJob
   PRIORITY_KEEP_ALIVE = OFFSET_PRI + 50
 
-  # Load historic data higher than batch job so it gets throughput
+  # download historic data higher priority than batch job so it gets throughput
   PRIORITY_DOWNLOAD_HISTORIC_DATA = OFFSET_PRI + 55
-  PRIORITY_LOAD_HISTORIC_DATA = OFFSET_PRI + 60
+  PRIORITY_FETCH_HISTORIC_DATA = OFFSET_PRI + 60
+
   PRIORITY_LOAD_FOOTBALL_DATA = OFFSET_PRI + 65
   # This don't involve connecting to the API, so lower tnan everything
   PRIORITY_INFER_GOAL_TIMES = OFFSET_PRI + 70
