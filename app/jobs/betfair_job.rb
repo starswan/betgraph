3fda5d88157e8b86b59ff515b4af3b35fa57cb2a
starswan <github@starswan.com>
2026-04-13 10:38:40 +0100

    increase retries

--- app/jobs/betfair_job.rb
+++ app/jobs/betfair_job.rb
@@ -9,7 +9,7 @@ class BetfairJob < ApplicationJob
   # this change appears to have been detrimental
   # retry_on Curl::Err::TimeoutError
   retry_on HTTPClient::ConnectTimeoutError, wait: ->(executions) { executions**2 }, attempts: 10
-  retry_on HTTPClient::ReceiveTimeoutError, wait: ->(executions) { executions**2 }, attempts: 10
+  retry_on HTTPClient::ReceiveTimeoutError, wait: ->(executions) { executions**2 }, attempts: 100
 
   def bc
     @@bc ||= BetfairLogin.new logger
