981c3890ab838d5e2056d108c9b870c46bfd021c
Stephen Dicks <github@starswan.com>
2026-07-21 20:26:12 +0100

    HTTPI SSL rertry (#801)

--- app/jobs/betfair_job.rb
+++ app/jobs/betfair_job.rb
@@ -8,6 +8,7 @@
 class BetfairJob < ApplicationJob
   retry_on HTTPClient::ConnectTimeoutError, wait: ->(executions) { executions**2 }, attempts: 10
   retry_on HTTPClient::ReceiveTimeoutError, wait: ->(executions) { executions**2 }, attempts: 100
+  retry_on HTTPI::SSLError, wait: ->(executions) { executions**2 }, attempts: 100
 
   def bc
     @@bc ||= BetfairLogin.new logger
